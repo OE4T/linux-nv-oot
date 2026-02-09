246e3a297a4e92b89d999a5cc2a15d729e291c5b
Meng Li <Meng.Li@windriver.com>
2026-02-09 16:45:54 +0800

    video: tegra: nvmap: remove iommu_present() checking
    Based on kernel upstream commit 6632863226d8("iommu: Remove
    iommu_present()"), it doesn’t need to check whether iommu is available
    or not anymore.
    
    Upstream-Status: Pending
    
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/video/tegra/nvmap/nvmap_dev.c
+++ drivers/video/tegra/nvmap/nvmap_dev.c
@@ -1493,8 +1493,7 @@ int __init nvmap_probe(struct platform_device *pdev)
 			generic_carveout_present = 1;
 
 	if (generic_carveout_present) {
-		if (!iommu_present(&platform_bus_type) &&
-			!nvmap_is_iommu_present())
+		if (!nvmap_is_iommu_present())
 			nvmap_convert_iovmm_to_carveout = 1;
 		else if (!of_property_read_bool(pdev->dev.of_node,
 				"dont-convert-iovmm-to-carveout"))
