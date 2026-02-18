4b8b05eb35123f06eccfc8322570615c48b9d7f5
Jon Hunter <jonathanh@nvidia.com>
2026-02-18 08:55:05 -0800

    drivers: Fix from_timer() for Linux v6.16
    In Linux v6.16, commit 41cb08555c41 ("treewide, timers: Rename
    from_timer() to timer_container_of()") renamed 'from_timer()' to
    'timer_container_of()'. Given that this is a macro we can simplfy see if
    the macro 'timer_container_of' is defined and if so use this otherwise
    fall back to 'from_timer()'. Update the necessary drivers to fix the
    build for Linux v6.16.
    
    JIRA LINQPJ14-60
    
    Upstream-Status: Backport
    Change-Id: I7f622b5d046a92da2ade755e6a697c1810f61275
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3383387
    (cherry picked from commit 320ec84efd492c0c2711c69104fabc30b4f15ecb)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3461850
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Brad Griffis <bgriffis@nvidia.com>
    (cherry picked from commit 96cae7f9dead812a1a8f342499278d3721636182)
    Signed-off-by: Matt Madison <matt@madison.systems>

--- drivers/nvpps/nvpps_main.c
+++ drivers/nvpps/nvpps_main.c
@@ -360,7 +360,12 @@ static void tsc_timer_callback(unsigned long data)
 #else /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0) */
 static void tsc_timer_callback(struct timer_list *t)
 {
-	struct nvpps_device_data *pdev_data = (struct nvpps_device_data *)from_timer(pdev_data, t, tsc_timer);
+	struct nvpps_device_data *pdev_data =
+#if defined(timer_container_of) /* Linux v6.16 */
+		timer_container_of(pdev_data, t, tsc_timer);
+#else
+		from_timer(pdev_data, t, tsc_timer);
+#endif
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0) */
 	uint32_t tsc_lock_status;
 	tsc_lock_status = readl(pdev_data->tsc_reg_map_base + TSC_LOCKING_STATUS_OFFSET);
@@ -394,7 +399,12 @@ static void nvpps_timer_callback(unsigned long data)
 #else /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0) */
 static void nvpps_timer_callback(struct timer_list *t)
 {
-        struct nvpps_device_data        *pdev_data = (struct nvpps_device_data *)from_timer(pdev_data, t, timer);
+	struct nvpps_device_data *pdev_data =
+#if defined(timer_container_of) /* Linux v6.16 */
+		timer_container_of(pdev_data, t, timer);
+#else
+		from_timer(pdev_data, t, timer);
+#endif
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0) */
 	/* get timestamps for this event */
 	nvpps_get_ts(pdev_data, 0);
