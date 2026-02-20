ab530fe61e092285870adf69373044eb991b5d4e
Jon Hunter <jonathanh@nvidia.com>
2026-02-20 08:22:28 -0800

    gpu/drm: tegra: Fix support for Linux v6.12
    In Linux v6.12, commit 641bb4394f40 ("fs: move FMODE_UNSIGNED_OFFSET to
    fop_flags") moves the setting of the FMODE_UNSIGNED_OFFSET into the
    various drivers and renames this flag. This change is causing various
    tests to fail because opening the DRM device is failing.
    
    Use the presence of the new FOP_UNSIGNED_OFFSET flag in the kernel to
    determine if we need to set this flag in the Tegra DRM driver.
    
    Bug 4876974
    
    Upstream-Status: Backport
    Change-Id: Ie72a9340187b1c206f0cda6d7e3dab44d603a41c
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nv-oot/+/3269063
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    (cherry picked from commit 00ab7abbb67d681bcac68fd9bcee90a05cc83ebc)
    Signed-off-by: Matt Madison <matt@madison.systems>

--- drivers/gpu/drm/tegra/drm.c
+++ drivers/gpu/drm/tegra/drm.c
@@ -806,7 +806,7 @@ static const struct file_operations tegra_drm_fops = {
 	.read = drm_read,
 	.compat_ioctl = drm_compat_ioctl,
 	.llseek = noop_llseek,
-#if defined(FOP_UNSIGNED_OFFSET)
+#if defined(NV_FOP_UNSIGNED_OFFSET_PRESENT) /* Linux v6.12 */
 	.fop_flags = FOP_UNSIGNED_OFFSET,
 #endif
 };
